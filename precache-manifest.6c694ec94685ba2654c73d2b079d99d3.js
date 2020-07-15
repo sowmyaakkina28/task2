471f9e26c8968c7c2ba7832c9a56eae586c4a5e5
sowmyaakkina <sowmyachowdary2001@gmail.com>
2020-07-15 13:55:21 +0530

    first commit

---
+++ precache-manifest.6c694ec94685ba2654c73d2b079d99d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc0266ad611b86d5487fa1c167e2da6",
+    "url": "/react-demo-profile/index.html"
+  },
+  {
+    "revision": "3f719e70f6b24b1f9a7b",
+    "url": "/react-demo-profile/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "ddb0b401a92d0eecd20d",
+    "url": "/react-demo-profile/static/css/main.bd26f739.chunk.css"
+  },
+  {
+    "revision": "3f719e70f6b24b1f9a7b",
+    "url": "/react-demo-profile/static/js/2.7109ddb5.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/react-demo-profile/static/js/2.7109ddb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddb0b401a92d0eecd20d",
+    "url": "/react-demo-profile/static/js/main.bc5157fc.chunk.js"
+  },
+  {
+    "revision": "c78111f1a01bc0ec23aa",
+    "url": "/react-demo-profile/static/js/runtime-main.87f12639.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-demo-profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
